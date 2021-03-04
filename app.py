553502a7a727557d09882a4223827acebc037a90
syrvanser <syrvanser@gmail.com>
2021-03-04 18:13:25 +0000

    Ivan2 (#6)
    * rewrote ngram model compression
    
    * added qc/wc

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 import os
 import logging
+import sys
 from datetime import datetime
 
 from ETL.preprocessing import Preprocessor
@@ -61,11 +63,11 @@ indexer.add_all_doc_ids(doc_ids)
 
 # Load index (for testing)
 indexer.index = indexer.load_index()
+qc = Query_Completer(n = 3)
+qc.load_model("./features/qc_model.pkl", "./features/qc_map_to_int.pkl",  "./features/qc_map_to_token.pkl")
 
-#qc = Query_Completer(n = 3)
-#qc.load_model("./features/qc_model.pkl", "./features/qc_map_to_int.pkl",  "./features/qc_map_to_token.pkl")
-#wc = Word_Completer()
-#wc.load_model("./features/wc_model.pkl")
+wc = Word_Completer()
+wc.load_model("./features/wc_model.pkl")
 
 @app.route("/")
 def handle_root():
@@ -162,7 +164,7 @@ def handle_artists():
             "id": artist.id,
             "artist": artist.name,
         } for artist in artists if query in artist.name.lower()]
-    return {"artists": results}
+    return {"results": results}
 
 @app.route('/api/songs/query_autocomplete')
 def handle_autocomplete():
@@ -176,8 +178,10 @@ def handle_autocomplete():
 
     # if ends with a space predict the next word, otherwise predict the rest of the current word
     if query[-1] == " ":
+        print("!", query[:-1], "!")
         results = qc.predict_next_token(query[:-1])
     else:
         results = wc.predict_token(query, 5)
-
+    if results == None:
+        results = []
     return {"suggestions": results}
--- features/ngram_model.py
+++ features/ngram_model.py
@@ -1,18 +1,15 @@
 from collections import Counter, defaultdict
 import numpy as np
-import dill
+import pickle
 import os
 import re
 import time
-from nltk.stem import PorterStemmer
-
 
 
 class Query_Completer():
     def __init__(self, n):
         """
         Initializes the Query_Completer class
-
         :param n: indicating the type of the ngram (n=3 --> trigram) (int)
         """
         self.n = n
@@ -23,12 +20,10 @@ class Query_Completer():
         self.mapping_to_token = {}
         self.last_mapping = -1 # Indicator which is the last mapping assigned to a token
 
-        self.stemmer = PorterStemmer() # Stemmer to stem identificators
 
     def tokenize_lyrics(self, lyrics):
         """
         Function which returns the tokenized and lowered lyrics
-
         :param lyrics: The lyrics to preprocess (str)
         :return line: Tokenized and lowered list of tokens (list)
         """
@@ -41,7 +36,6 @@ class Query_Completer():
     def create_ngram(self, sentence):
         """
         Function which returns the ngrams of the sentence
-
         :param sentence: Tokens (list)
         :return ngram_list: List containing all the possible ngrams of the sentence in tuples of n items (list of tuples)
         """
@@ -76,7 +70,6 @@ class Query_Completer():
     def add_single_lyric(self, lyrics):
         """
         Function which adds the ngrams of the given sentence to the model
-
         :param lyrics: The lyrics (str)
         """
         # Tokenize the lyrics 
@@ -84,9 +77,6 @@ class Query_Completer():
         ngram_list = self.create_ngram(token_list)
 
         for tokens in ngram_list:
-            # Stem the identifiers and leave the final word as is
-            tokens = [self.stemmer.stem(token) for token in tokens[:-1]] + [tokens[-1]]
-
             mapping = [0]*len(tokens) # create mapping to int to pass to model 
             for idx, token in enumerate(tokens):
 
@@ -109,7 +99,6 @@ class Query_Completer():
     def add_lyrics(self, lyric_list):
         """
         Function which adds the ngrams of the given sentences to the model
-
         :param lyric_list: List of lyrics (list of strings)
         """
         for lyrics in lyric_list:
@@ -119,9 +108,7 @@ class Query_Completer():
     def predict_next_token(self, current_query):
         """
         Function which returns most probable next words based on the model
-
         :param current_query: The currently inserted query (str)
-
         :return sorted_result: Sorted list of most probable continuations + sentence (list of str)
         """
         
@@ -135,7 +122,6 @@ class Query_Completer():
 
         # extracts the last m = n-1 tokens from the token list
         last_m_tokens = query_token_list[-(self.n-1):]
-        last_m_tokens = [self.stemmer.stem(token) for token in last_m_tokens] 
 
         query_mapping = [0]*len(last_m_tokens)
 
@@ -156,40 +142,39 @@ class Query_Completer():
     def save_model(self, model_filepath = "qc_model.pkl", map_to_int_filepath = "qc_map_to_int.pkl", map_to_token_filepath = "qc_map_to_token.pkl"):
         """
         Function which saved the models in the file
-
         :param model_filepath: Filepath to the model - has to be of type .pkl (str)
         :param map_to_int_filepath: Filepath to the mapping from token to int - has to be of type .pkl (str)
         :param map_to_token_filepath: Filepath to the mapping from int to token - has to be of type .pkl (str)
         """
         with open(model_filepath, 'wb') as file:
-            dill.dump(self.model, file)
+            pickle.dump(self.model, file)
 
         with open(map_to_int_filepath, 'wb') as file:
-            dill.dump(self.mapping_to_int, file)
+            pickle.dump(self.mapping_to_int, file)
 
         with open(map_to_token_filepath, 'wb') as file:
-            dill.dump(self.mapping_to_token, file)
+            pickle.dump(self.mapping_to_token, file)
 
         
     def load_model(self, model_filepath = "qc_model.pkl", map_to_int_filepath = "qc_map_to_int.pkl", map_to_token_filepath = "qc_map_to_token.pkl"):
         """
         Function which loades the model in the file
-
         :param model_filepath: Filepath to the model - has to be of type .pkl (str)
         :param map_to_int_filepath: Filepath to the mapping from token to int - has to be of type .pkl (str)
         :param map_to_token_filepath: Filepath to the mapping from int to token - has to be of type .pkl (str)
         """
 
         with open(model_filepath, 'rb') as file:
-            self.model = dill.load(file)
+            self.model = pickle.load(file)
 
         with open(map_to_int_filepath, 'rb') as file:
-            self.mapping_to_int = dill.load(file)
+            self.mapping_to_int = pickle.load(file)
 
         with open(map_to_token_filepath, 'rb') as file:
-            self.mapping_to_token = dill.load(file)
+            self.mapping_to_token = pickle.load(file)
 
         self.last_mapping = max(self.mapping_to_int, key = self.mapping_to_int.get)
+
 
     def reduce_model(self, cutoff):
         """
@@ -197,55 +182,71 @@ class Query_Completer():
 
         :param cutoff: Number indicating the least amount of occurrences to keep
         """
-        to_delete = []
-        for key, subdict in self.model.items():
-
+       
+        for _, subdict in self.model.items():
             # Count how many occurences an identifier has
-            count = 0
-            for _, cur_count in subdict.items():
-                count += cur_count
+            to_remove = []
+            for cur_key, cur_count in subdict.items():
+                if cur_count < cutoff:
+                    to_remove.append(cur_key)
+        
+            for x in to_remove:
+                del subdict[x]
 
-            if count < cutoff:
-                to_delete.append(key)
+        to_remove = []
+        for key in self.model:
+            if len(self.model[key]) == 0:
+                to_remove.append(key)
 
-        # Delete the relevant keys from the model
-        for key in to_delete:
-            del self.model[key]
+        for x in to_remove:
+            del self.model[x]
+        
+        vals = set()
+        for key in self.model:
+            vals.update(list(key))
+            vals.update(list(self.model[key].keys()))
 
+        to_delete = []
+        for id in self.mapping_to_token:
+            if id not in vals:
+                to_delete.append(id)
+        for x in to_delete:
+            del self.mapping_to_int[self.mapping_to_token[x]]    
+            del self.mapping_to_token[x]
 
-'''
 # Set path as needed for Query_Completer class
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-qc = Query_Completer(n = 3)
-'''
-'''
-begin = time.time()
-for ids, (idx, row) in enumerate(data.iterrows()):
-    if ids%1000 == 0:
-        print(f"{ids} out of {data.shape[0]}", end='\r')
-    qc.add_single_lyric(row["SongLyrics"])
+# abspath = os.path.abspath(__file__)
+# dname = os.path.dirname(abspath)
+# os.chdir(dname)
+# qc = Query_Completer(n = 3)
+#qc.load_model("qc_model.pkl")
 
-begin2 = time.time()
-qc.reduce_model(5)
-print(f"cutoff took: {time.time() - begin2}")
+# import pandas as pd
+# data = pd.read_csv("data-song_v1.csv")
+# begin = time.time()
+# for ids, (idx, row) in enumerate(data.iterrows()):
+#     if ids%1000 == 0:
+#         print(f"{ids} out of {data.shape[0]}", end='\r')
+#     qc.add_single_lyric(row["SongLyrics"])
+# print(f"{data.shape[0]} out of {data.shape[0]}", end='\r')
+# begin2 = time.time()
+# #qc.load_model("qc_model.pkl")
 
-qc.save_model("qc_model.pkl")
-print(f"Training and saving took: {time.time() - begin}")
-'''
-'''
+#qc.reduce_model(6)
+# print(f"cutoff took: {time.time() - begin2}")
+
+#qc.save_model("qc_model.pkl")
+# print(f"Training and saving took: {time.time() - begin}")
+
 # Reload the model and make predictions
-qc.load_model("qc_model.pkl")
-
-print("Predicting")
-begin = time.time()
-print(qc.predict_next_token("deux trois"))
-print(qc.predict_next_token("Cinq six sept"))
-print(qc.predict_next_token("did it"))
-print(qc.predict_next_token("HALLO I BIMS"))
-print(qc.predict_next_token("Oops I"))
-print(qc.predict_next_token("My loneliness"))
-print(qc.predict_next_token("Es ragen aus ihrem aufgeschlitzten Bauch"))
-print(f"Prediction took took: {time.time() - begin}")
-'''
+# qc.load_model("qc_model.pkl")
+# print("Predicting")
+# begin = time.time()
+#print(qc.predict_next_token("deux trois"))
+# print(qc.predict_next_token("Cinq six sept"))
+# print(qc.predict_next_token("did it"))
+# print(qc.predict_next_token("HALLO I BIMS"))
+# print(qc.predict_next_token("Oops I"))
+# print(qc.predict_next_token("My loneliness"))
+# print(qc.predict_next_token("Es ragen aus ihrem aufgeschlitzten Bauch"))
+# print(f"Prediction took took: {time.time() - begin}")
